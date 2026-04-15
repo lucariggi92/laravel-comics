09f818dad2693771c132a0a2603f9531210f9900
lucariggi92 <riggiluca92@gmail.com>
2026-04-15 09:58:06 +0200

    feat: partials folder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,19 @@
-ciao
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HomePage</title>
+</head>
+<body>
+    @include("partials.header")
+    <br>
+    @php
+    $saluto ="Ciao"
+    @endphp
+    
+    {{$saluto}}
+    <br>
+      @include("partials.footer")
+</body>
+</html>
