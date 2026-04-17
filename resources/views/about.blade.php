2a95ed9cb2fea8e28471d98fba832163295552d2
lucariggi92 <riggiluca92@gmail.com>
2026-04-17 12:36:26 +0200

    feat: completed

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends("layouts.master")
+
+@section('hero banner')
+<section class="bg-herobanner"></section>
+@endsection
+
+@section('grid card')
+<section class="bg-black py-5">
+    <div class="container text-light">
+        <h1>About DC Comics</h1>
+        <p>DC Comics è una delle case editrici di fumetti più famose al mondo...</p>
+    </div>
+</section>
+@endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,79 @@
 
+<footer class="text-light  "> 
+    <section class="bg-footer">
 
-<footer className="text-white ">
-           <div class="bg-footer"></div>
-             footer
-        </footer>
+    
+        <img src="{{ asset('img/dc-logo-bg.png') }}" alt="" class="bg-logo">
+        <div class="container mb-1 mt-4">
+            <div class=" d-flex flex-wrap gap-5">
+                
+                <div class="footer-col">
+                    <h2 class="uppercase">dc comics</h2>
+                    <ul class="list-unstyled">
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+
+                    <h2 class="uppercase">shop</h2>
+                    <ul class="list-unstyled">
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+
+                <div class="footer-col">
+                <h2 class="uppercase">dc</h2>
+                    <ul class="list-unstyled">
+                        <li>Terms Of Use</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                    
+                    </ul>
+                </div>
+
+                <div class="footer-col">
+                    <h2 class="uppercase">sites</h2>
+                    <ul class="list-unstyled">
+                        <li>Terms Of Use</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+</section>
+    <section class="bg-footer-luca p-5">
+        <div class="container d-flex space-between">
+            <a href="/"><button type="button" class="btn btn-outline-primary">sign-up now!</button></a>
+            <div class="uppercase d-flex align-items-center ">
+                <p class="m-0 px-3 text-footer-luca">follow us</p>
+                
+                <a href="/"><img src="{{'img/footer-facebook.png' }}" alt="" class="footer-img"></a>
+                  <a href="/"><img src="{{'img/footer-twitter.png' }}" alt="" class="footer-img"></a>
+                  <a href="/"> <img src="{{'img/footer-pinterest.png' }}" alt="" class="footer-img"></a>
+                  <a href="/"> <img src="{{'img/footer-periscope.png' }}" alt="" class="footer-img"></a>
+                   <a href="/"><img src="{{'img/footer-youtube.png' }}" alt="" class="footer-img"></a>
+            </div>
+                
+        </div>
+     
+    </section>
+
+    
+</footer>
